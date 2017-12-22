45104f574bc903d1846c3658e887d47cadea6581
Jonathan Sick <jsick@lsst.org>
2017-12-22 14:25:27 -0700

    Add make version command
    Demonstrate adding a flask subcommand that, in this case, prints the app
    version string.

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-.PHONY: help server
+.PHONY: help server version
 
 help:
 	@echo "Run these commands in separate shells:"
 	@echo "  make server      (start the Flask app)"
+	@echo "  make version     (print app version)"
 
 server:
 	FLASK_APP=wwwlsstio:app flask run
+
+version:
+	FLASK_APP=wwwlsstio:app flask version
--- wwwlsstio/__init__.py
+++ wwwlsstio/__init__.py
@@ -1,7 +1,7 @@
 """Flask app for generating www.lsst.io.
 """
 
-__all__ = ['__version__', 'app']
+__all__ = ['__version__', 'app', 'version']
 
 from .appfactory import create_app
 from .version import get_version
@@ -10,3 +10,10 @@ from .version import get_version
 __version__ = get_version()
 
 app = create_app()
+
+
+@app.cli.command()
+def version():
+    """Flask CLI subcommand for printing the app version.
+    """
+    print(get_version())
