d87ac7acb8ef92b2653eec7159c838d4f7d0369e
Jonathan Sick <jsick@lsst.org>
2017-12-27 10:32:55 -0700

    Demonstrate scss compilation
    - gulp scss command using gulp-sass
    - Destination directory for built css is the static directory that is
      used by default by Flask as a static asset directory.
    - Add a Makefile target for scss compilation

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
-.PHONY: help server version
+.PHONY: help server scss version
 
 help:
 	@echo "Run these commands in separate shells:"
 	@echo "  make server      (start the Flask app)"
+	@echo "  make scss        (compile scss)"
 	@echo "  make version     (print app version)"
 
 server:
 	FLASK_APP=wwwlsstio:app flask run
+
+scss:
+	gulp scss
 
 version:
 	FLASK_APP=wwwlsstio:app flask version
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,5 @@
 import gulp from 'gulp';
+import sass from 'gulp-sass';
 import minimist from 'minimist';
 
 // parse command line options
@@ -6,5 +7,25 @@ import minimist from 'minimist';
 const options = minimist(process.argv);
 const env = options.env || 'dev';
 
+
+// Source and build directories
+const dirs = {
+  scss: 'scss', // Scss source directory
+  static: 'wwwlsstio/static', // Flask app static assets directory
+};
+
+// Sass modules installed via npm: these are added to node-sass's search paths.
+const sassIncludes = [
+  'node_modules',
+];
+
 // Print the current environment setting
 gulp.task('environment', () => console.log(`${env}`));
+
+// Compile Scss
+// gulp scss
+gulp.task('scss', () => {
+  return gulp.src(`${dirs.scss}/app.scss`)
+    .pipe(sass.sync({ includePaths: sassIncludes }).on('error', sass.logError))
+    .pipe(gulp.dest(dirs.static))
+});
