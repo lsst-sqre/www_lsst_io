9ebfb75021eb46dd85bbe7fc447bb7f496df7b58
Jonathan Sick <jsick@lsst.org>
2017-12-28 12:16:15 -0700

    Add clean-css to scss Gulp task
    - gulp-css-clean 3.9.0
    - css-clean 4.1.9
    - Permit different css optimizations based on the dev or prod
      environments with the cleanCssConfig map.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,5 @@
 import gulp from 'gulp';
+import cleanCSS from 'gulp-clean-css';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 import minimist from 'minimist';
@@ -20,18 +21,38 @@ const sassIncludes = [
   'node_modules',
 ];
 
+// clean-css configurations, mapped to environment
+// https://github.com/jakubpawlowicz/clean-css
+let cleanCssConfig = new Map();
+cleanCssConfig.set('dev', {
+  compatibility: '*',
+  level: 2,
+  format: 'beautify'
+});
+cleanCssConfig.set('prod', {
+  compatibility: '*',
+  level: 2
+});
+
 // Print the current environment setting
 gulp.task('environment', () => console.log(`${env}`));
 
 // Compile Scss
 // gulp scss
 gulp.task('scss', () => {
-  return gulp.src(`${dirs.scss}/app.scss`)
+  let stream = gulp.src(`${dirs.scss}/app.scss`)
     // Initialize sourcemaps
-    .pipe(env === 'dev' ? sourcemaps.init() : gutil.noop())
+    .pipe(sourcemaps.init())
     // Compile scss
     .pipe(sass.sync({ includePaths: sassIncludes }).on('error', sass.logError))
+    // Optimize css
+    .pipe(cleanCSS(cleanCssConfig.get(env)));
+
+  if (env === 'dev') {
     // Write out sourcemaps
-    .pipe(env === 'dev' ? sourcemaps.write() : gutil.noop())
-    .pipe(gulp.dest(dirs.static))
+    stream.pipe(sourcemaps.write());
+  }
+
+  stream.pipe(gulp.dest(dirs.static));
+  return stream;
 });
