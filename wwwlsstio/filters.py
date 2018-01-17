3bb94752aa8116e8bcaaaba33577a9b769998422
Jonathan Sick <jsick@lsst.org>
2018-01-17 15:05:25 -0700

    Include author list of project tile
    - Add a format_schema_author_name Jinja filter to take a schema.org
      author node and convert it into a display name.
    
    - Format authors as a <ul>, but the .c-author-list display it as an
      inline list with commas (and an oxford comma) between author names.

--- wwwlsstio/filters.py
+++ wwwlsstio/filters.py
@@ -10,6 +10,8 @@ def register_filters(app):
     """Add filters to the Flask application's Jinja environment.
     """
     app.jinja_env.filters['strip_protocol'] = strip_protocol
+    app.jinja_env.filters['format_schema_author_name'] \
+        = format_schema_author_name
 
 
 def strip_protocol(url):
@@ -20,3 +22,14 @@ def strip_protocol(url):
     """
     parsed_url = urlparse(url)
     return parsed_url.netloc + parsed_url.path
+
+
+def format_schema_author_name(author):
+    """For a JSON schema.org author node into a name
+    """
+    if 'name' in author:
+        return author['name']
+    else:
+        # NOTE: there may be other ways to encode a name in schema.org JSON,
+        # so this function may have to be expanded to deal with those.
+        return 'unknown'
