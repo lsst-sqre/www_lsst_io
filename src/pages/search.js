b01caca795df238dba1026f4f333885265104f7f
Jonathan Sick <jsick@lsst.org>
2020-04-06 13:50:17 -0400

    Add bare bones searchbox + hits widget

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+
+const searchClient = algoliasearch(
+  '0OJETYIVL5',
+  'b7bd2f1080a5c4fe5eee502462bcc9d3'
+);
 
 const AdvancedSearchPage = () => (
   <Layout>
     <SEO title="Advanced search" />
     <h1>Advanced search</h1>
+
+    <InstantSearch searchClient={searchClient} indexName="document_dev">
+      <SearchBox />
+      <Hits />
+    </InstantSearch>
   </Layout>
 );
 
