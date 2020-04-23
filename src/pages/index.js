6226c30a3c3b9457448e9957630e5b641dd6194c
Jonathan Sick <jsick@lsst.org>
2020-04-23 16:03:52 -0400

    Remove the demo Image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
@@ -10,9 +9,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Find Rubin Observatory docs and open source</h1>
     <Link to="/search/">Check out the advanced search page.</Link>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
   </Layout>
 );
 
