98cd96001ef4d6b28c01e27ad0869caaf82c4830
Jonathan Sick <jsick@lsst.org>
2020-04-27 14:18:31 -0400

    Add hero search to home page
    This gives us a large search box that we can use to drive search to the
    home page.
    
    The styling here needs to be refactored.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,61 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+
+const StyledHeroSearch = styled.div`
+  font-size: 2rem;
+
+  form {
+    display: flex;
+  }
+
+  input[type='submit'] {
+    margin-left: 1rem;
+  }
+
+  label span {
+    // visually hidden (refactor this)
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+  }
+`;
+
+const SearchBox = styled.div`
+  flex: 1 1 0;
+
+  input {
+    width: 100%;
+  }
+`;
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Find Rubin Observatory docs and open source</h1>
-    <Link to="/search/">Check out the advanced search page.</Link>
+    <p>
+      <Link to="/search/">Check out the advanced search page.</Link>
+    </p>
+
+    <StyledHeroSearch role="search">
+      <form>
+        <SearchBox>
+          <label htmlFor="search">
+            <input type="search" id="search" name="search" />
+            <span>Search</span>
+          </label>
+        </SearchBox>
+        <input type="submit" value="Search" />
+      </form>
+    </StyledHeroSearch>
   </Layout>
 );
 
