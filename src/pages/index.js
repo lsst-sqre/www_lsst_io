f3d64410f84c6d54069c521760a890c4fda951ee
Jonathan Sick <jsick@lsst.org>
2020-04-28 11:03:30 -0400

    Refactor HeroSearchForm into its own component
    This keeps the search form off the index page itself and also fixes the
    styling issues that came up when we modified the form to add handlers.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,9 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-
-const StyledHeroSearch = styled.div`
-  font-size: 2rem;
-
-  form {
-    display: flex;
-  }
-
-  input[type='submit'] {
-    margin-left: 1rem;
-  }
-
-  label span {
-    // visually hidden (refactor this)
-    border: 0;
-    clip: rect(0 0 0 0);
-    height: 1px;
-    margin: -1px;
-    overflow: hidden;
-    padding: 0;
-    position: absolute;
-    width: 1px;
-  }
-`;
-
-const SearchBox = styled.div`
-  flex: 1 1 0;
-
-  input {
-    width: 100%;
-  }
-`;
+import HeroSearchForm from '../components/heroSearchForm';
 
 const IndexPage = () => (
   <Layout>
@@ -45,17 +13,7 @@ const IndexPage = () => (
       <Link to="/search/">Check out the advanced search page.</Link>
     </p>
 
-    <StyledHeroSearch role="search">
-      <form>
-        <SearchBox>
-          <label htmlFor="search">
-            <input type="search" id="search" name="search" />
-            <span>Search</span>
-          </label>
-        </SearchBox>
-        <input type="submit" value="Search" />
-      </form>
-    </StyledHeroSearch>
+    <HeroSearchForm role="search" />
   </Layout>
 );
 
