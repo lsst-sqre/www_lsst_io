73b5da7b29b3f0dc0860cbbc938e5b265e388fee
Jonathan Sick <jsick@lsst.org>
2020-05-08 19:27:34 -0400

    Include light bulb icon with ThemeToggleButton
    This is a first shot at spiffing up the toggle button. I'm using the
    light icon from Refactoring UI and colouring it based on the theme. I
    think we'll want to do more work on this; perhaps increase the size?
    Make it icon-only? Sun/moon?

--- src/components/themeToggle.js
+++ src/components/themeToggle.js
@@ -4,17 +4,43 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withTheme } from 'styled-components';
+import styled, { withTheme } from 'styled-components';
+import themeFunc from 'styled-theming';
 
 import { useTheme } from '../context/themeContext';
+
+import { yellow, neutral } from '../design/color';
+import Button from './buttons';
+import LightIcon from '../icons/light.svg';
+
+const variations = themeFunc('scheme', {
+  light: yellow['900'],
+  dark: yellow['200'],
+});
+
+const StyledLightIcon = styled(LightIcon)`
+  // Technique comes from Every Layout
+  width: 0.75em;
+  width: 1cap;
+  height: 0.75em;
+  height: 1cap;
+
+  .primary {
+    fill: ${variations};
+  }
+
+  .secondary {
+    fill: ${neutral['300']};
+  }
+`;
 
 const ThemeToggleButton = ({ theme }) => {
   const { toggleColorScheme } = useTheme();
 
   return (
-    <button type="button" onClick={() => toggleColorScheme()}>
-      {theme.scheme === 'dark' ? 'Light mode' : 'Dark mode'}
-    </button>
+    <Button dir="ltr" type="button" onClick={() => toggleColorScheme()}>
+      <StyledLightIcon /> {theme.scheme === 'dark' ? 'Lighten' : 'Darken'}
+    </Button>
   );
 };
 
