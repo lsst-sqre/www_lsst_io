c7da7d5c81f72f00a22da26e4446c1030dc73b7b
Jonathan Sick <jsick@lsst.org>
2020-05-12 14:16:49 -0400

    Add a background image to the hero search
    - Background image is LSST stills-0014.jpg from "Summit Multimedia Visit
      March 2017" on gallery.lsst.org.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,102 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql, StaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import HeroSearchForm from '../components/heroSearchForm';
+import PageContentContainer from '../components/pageContentContainer';
+
+import { neutral } from '../design/color';
+
+const BackgroundSection = ({ className, children }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "lsst-stills-0014.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const imageData = data.desktop.childImageSharp.fluid;
+
+      return (
+        <BackgroundImage
+          Tag="section"
+          className={className}
+          fluid={imageData}
+          backgroundColor={neutral['900']}
+        >
+          {children}
+        </BackgroundImage>
+      );
+    }}
+  />
+);
+
+BackgroundSection.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+const StyledBackgroundSection = styled(BackgroundSection)`
+  // Full-width in a contrained parent
+  // https://css-tricks.com/full-width-containers-limited-width-parents/
+  width: 100vw;
+  position: relative;
+  left: 50%;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
+
+  // Background image
+  background-position: bottom center;
+  background-size: cover;
+
+  // Effectively a "reversed" block; doesn't change based on theme
+  color: ${neutral['100']};
+`;
+
+/*
+ * Container for content within the BackgroundSection.
+ *
+ * Using flexbox to center the hero content.
+ */
+const StyledSearchContainer = styled(PageContentContainer)`
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 70vh; // Give a peak of content down the page
+  }
+
+  .principal {
+    margin-top: auto;
+    margin-bottom: auto;
+  }
+`;
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Find Rubin Observatory docs and open source</h1>
 
-    <HeroSearchForm role="search" />
+    <StyledBackgroundSection>
+      <StyledSearchContainer>
+        <div className="wrapper">
+          <div className="principal">
+            <h1>Find Rubin Observatory docs and open source</h1>
+
+            <HeroSearchForm role="search" />
+          </div>
+        </div>
+      </StyledSearchContainer>
+    </StyledBackgroundSection>
   </Layout>
 );
 
