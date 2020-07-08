f002b6cb96d15100739d1add5f641f851e7260cd
Jonathan Sick <jsick@lsst.org>
2020-07-08 14:21:35 -0400

    Format existing comments for consistency

--- src/components/globalStyles.js
+++ src/components/globalStyles.js
@@ -10,8 +10,8 @@ import { fontFamily } from '../design/type';
  */
 const GlobalStyle = createGlobalStyle`
   /*
-  * CSS reset via normalize.
-  */
+   * CSS reset via normalize.
+   */
   ${normalize()}
 
   html {
@@ -19,34 +19,37 @@ const GlobalStyle = createGlobalStyle`
   }
 
   /*
-  * Inherit border-box sizing from html
-  * https://css-tricks.com/inheriting-box-sizing-probably-slightly-better-best-practice/
-  */
+   * Inherit border-box sizing from html
+   * https://css-tricks.com/inheriting-box-sizing-probably-slightly-better-best-practice/
+   */
   *,
   *:before,
   *:after {
     box-sizing: inherit;
   }
 
-  /* Disable artifical scaling, but let user zoom if needed.
-  * Flexible Typesetting, Tim Brown, ch 2.
-  */
+  /*
+   * Disable artifical scaling, but let user zoom if needed.
+   * Flexible Typesetting, Tim Brown, ch 2.
+   */
   @viewport {
     width: device-width;
   }
 
-  /* Reinforce that we're respecting the user's ability to set a default
-  * font size. The rem unit now becomes relative to this.
-  * Flexible Typesetting, Tim Brown, ch 2 and 4
-  */
   :root {
+    /*
+     * Reinforce that we're respecting the user's ability to set a default
+     * font size. The rem unit now becomes relative to this.
+     * Flexible Typesetting, Tim Brown, ch 2 and 4
+     */
     font-size: 1.1rem;
   }
 
-  /* System font stacks (for now).
-  * https://css-tricks.com/snippets/css/system-font-stack/
-  */
   body {
+    /*
+     * System font stacks (for now).
+     * https://css-tricks.com/snippets/css/system-font-stack/
+     */
     ${fontFamily.sans}
     line-height: 1.45;
     color: ${textColor};
