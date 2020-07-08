af58c8031524cb4673d5244ea956af43c86fe043
Jonathan Sick <jsick@lsst.org>
2020-07-08 15:38:40 -0400

    Add CSS properties for colour theming
    - Create a custom property for every named colour in our palette (copied
      from design/color).
    - Create semantically-named CSS custom properties for what used to be in
      design/theme.
    - Add dark mode variants of the semantic colour properties in a
      body.dark selector. This will be compatible with
      https://github.com/insin/gatsby-plugin-dark-mode#readme

--- src/components/globalStyles.js
+++ src/components/globalStyles.js
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 import { normalize } from 'polished';
 
-import { backgroundColor, textColor, linkColor } from '../design/theme';
 import { fontFamily } from '../design/type';
 
 /*
@@ -47,17 +46,120 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     /*
+     * Named colours
+     * These colours come from the Refactoring UI book, Palette 7.
+     * https://refactoringui.com/book/
+     *
+     * Colours are arranged from light to dark.
+     */
+
+    /* Neutrals */
+    --c-neutral-050: #FAF9F7;
+    --c-neutral-100: #E8E6E1;
+    --c-neutral-200: #D3CEC4;
+    --c-neutral-300: #B8B2A7;
+    --c-neutral-400: #A39E93;
+    --c-neutral-500: #857F72;
+    --c-neutral-600: #625D52;
+    --c-neutral-700: #504A40;
+    --c-neutral-800: #423D33;
+    --c-neutral-900: #27241D;
+
+    /* Primary cyan */
+    --c-cyan-050: #E0FCFF;
+    --c-cyan-100: #BEF8FD;
+    --c-cyan-200: #87EAF2;
+    --c-cyan-300: #54D1DB;
+    --c-cyan-400: #38BEC9;
+    --c-cyan-500: #2CB1BC;
+    --c-cyan-600: #14919B;
+    --c-cyan-700: #0E7C86;
+    --c-cyan-800: #0A6C74;
+    --c-cyan-900: #044E54;
+
+    /* Blue */
+    --c-blue-050: #DCEEFB;
+    --c-blue-100: #B6E0FE;
+    --c-blue-200: #84C5F4;
+    --c-blue-300: #62B0E8;
+    --c-blue-400: #4098D7;
+    --c-blue-500: #2680C2;
+    --c-blue-600: #186FAF;
+    --c-blue-700: #0F609B;
+    --c-blue-800: #0A558C;
+    --c-blue-900: #003E6B;
+
+    /* Red */
+    --c-red-050: #FFEEEE;
+    --c-red-100: #FACDCD;
+    --c-red-200: #F29B9B;
+    --c-red-300: #E66A6A;
+    --c-red-400: #D64545;
+    --c-red-500: #BA2525;
+    --c-red-600: #A61B1B;
+    --c-red-700: #911111;
+    --c-red-800: #780A0A;
+    --c-red-900: #610404;
+
+    /* Yellow */
+    --c-yellow-050: #FFFAEB;
+    --c-yellow-100: #FCEFC7;
+    --c-yellow-200: #F8E3A3;
+    --c-yellow-300: #F9DA8B;
+    --c-yellow-400: #F7D070;
+    --c-yellow-500: #E9B949;
+    --c-yellow-600: #C99A2E;
+    --c-yellow-700: #A27C1A;
+    --c-yellow-800: #7C5E10;
+    --c-yellow-900: #513C06;
+
+    /* Teal */
+    --c-teal-050: #EFFCF6;
+    --c-teal-100: #C6F7E2;
+    --c-teal-200: #8EEDC7;
+    --c-teal-300: #65D6AD;
+    --c-teal-400: #3EBD93;
+    --c-teal-500: #27AB83;
+    --c-teal-600: #199473;
+    --c-teal-700: #147D64;
+    --c-teal-800: #0C6B58;
+    --c-teal-900: #014D40;
+
+    /*
+     * Semantic colours in default (light) theme
+     */
+    --c-text: var(--c-neutral-900);
+    --c-background: var(--c-neutral-050);
+    --c-link: var(--c-blue-600);
+    --c-reversed-text: var(--c-neutral-100);
+    --c-reversed-background: var(--c-neutral-900);
+    --c-reversed-link: var(--c-blue-100);
+    --c-tag-text: var(--c-neutral-700);
+    --c-tag-background: var(--c-neutral-100);
+
+    /*
      * System font stacks (for now).
      * https://css-tricks.com/snippets/css/system-font-stack/
      */
     ${fontFamily.sans}
     line-height: 1.45;
-    color: ${textColor};
-    background-color: ${backgroundColor};
+    color: var(--c-text);
+    background-color: var(--c-background);
+  }
+
+  body.dark {
+    --c-text: var(--c-neutral-100);
+    --c-background: var(--c-neutral-900);
+    --c-link: var(--c-blue-100);
+    --c-reversed-text: var(--c-neutral-100);
+    --c-reversed-background: var(#000000);
+    --c-reversed-link: var(--c-blue-100);
+    --c-tag-text: var(--c-neutral-100);
+    --c-tag-background: var(--c-neutral-700);
   }
 
   a {
-    color: ${linkColor}
+    color: var(--c-link);
   }
 `;
 
