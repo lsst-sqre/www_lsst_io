c1c6bd706a5988e101eecbc6bba87c7de001b9eb
Jonathan Sick <jsick@lsst.org>
2020-07-09 13:31:39 -0400

    Switch to functional component

--- src/components/themeToggle.js
+++ src/components/themeToggle.js
@@ -6,32 +6,29 @@ import React from 'react';
 import { ThemeToggler } from 'gatsby-plugin-dark-mode';
 
 /* eslint-disable */
-class ThemeToggle extends React.Component {
-  render() {
-    return (
-      <ThemeToggler>
-        {({ theme, toggleTheme }) => {
-          // Don't render anything at compile time. Deferring rendering until we
-          // know which theme to use on the client avoids incorrect initial
-          // state being displayed.
-          if (theme == null) {
-            return null;
-          }
-          return (
-            <label>
-              <input
-                type="checkbox"
-                onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
-                checked={theme === 'dark'}
-              />{' '}
-              Dark mode
-            </label>
-          );
-        }}
-      </ThemeToggler>
-    );
-  }
-}
+/* jsx-a11y/label-has-for jsx-a11y/label-has-associated-control */
+const ThemeToggle = () => (
+  <ThemeToggler>
+    {({ theme, toggleTheme }) => {
+      // Don't render anything at compile time. Deferring rendering until we
+      // know which theme to use on the client avoids incorrect initial
+      // state being displayed.
+      if (theme == null) {
+        return null;
+      }
+      return (
+        <label>
+          <input
+            type="checkbox"
+            onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+            checked={theme === 'dark'}
+          />{' '}
+          Dark mode
+        </label>
+      );
+    }}
+  </ThemeToggler>
+);
 /* eslint-enable */
 
 export default ThemeToggle;
