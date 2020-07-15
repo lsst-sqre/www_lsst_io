d954ff07821159debdc9417d16ff98000b2de16c
Jonathan Sick <jsick@lsst.org>
2020-07-15 17:22:07 -0400

    Set PWA's color schema and name
    This customizes the start's settings to fit Rubin Observatory branding.
    Still need to set the icon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,11 +19,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Vera Rubin Observatory Documentation`,
+        short_name: `Rubin Docs`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#E0FCFF`,
+        theme_color: `#0A6C74`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
