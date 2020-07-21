4eca37a3efcb125d0eb5800f6a53a2c4910cbb00
Jonathan Sick <jsick@lsst.org>
2020-07-21 15:37:52 -0400

    Remove elevations and spacing modules
    These are replaced by CSS custom properties.

--- src/design/spacing.js
+++
@@ -1,9 +0,0 @@
-/*
- * Spaced-related tokens.
- */
-
-/* Max width of the content container */
-export const mainWidth = '60em';
-
-export const regularMarginRem = 1;
-export const mediumMarginRem = 2;
