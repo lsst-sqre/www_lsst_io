307313c2182d37e1bef7ae3424a8e9d89f1c2b09
Jonathan Sick <jsick@lsst.org>
2021-09-16 12:14:25 -0400

    Switch to using internal component state
    Rather than using algoliasearch as the state for the component, we're
    now maintaining the component's state internally. This solves the
    problem of the UI results bouncing rapidly when someone tries to
    manipulate the text fields --- often resulting in it becoming impossible
    to use the component.
    
    Now we have an explicit submission button to send the component's new
    value to algolia. Perhaps in the future we can change this to submit
    after a delay of inactivity, or when the component loses focus? For now,
    this is the most reliable interface.
    
    Still needs work to style the button and form.

--- src/components/instantsearch/dateRangeInput.js
+++ src/components/instantsearch/dateRangeInput.js
@@ -3,7 +3,7 @@
  * a range of dates.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connectRange } from 'react-instantsearch-dom';
 import DateRangePicker from '@wojtekmaj/react-daterange-picker';
@@ -11,28 +11,42 @@ import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 /**
  * Min/max date range selector component.
  */
-const DateRangeInputCore = ({ currentRefinement, refine }) => (
-  <div>
-    <DateRangePicker
-      disableCalendar
-      rangeDivider=" to  "
-      onChange={(newValue) =>
-        refine({
-          min: newValue[0].getTime() / 1000,
-          max: newValue[1].getTime() / 1000,
-        })
-      }
-      value={[
-        new Date(currentRefinement.min * 1000),
-        new Date(currentRefinement.max * 1000),
-      ]}
-    />
-  </div>
-);
+const DateRangeInputCore = ({ currentRefinement, refine, min, max }) => {
+  const [dateRange, onChangeDateRange] = useState([
+    new Date(currentRefinement.min * 1000),
+    new Date(currentRefinement.max * 1000),
+  ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    refine({
+      min: dateRange[0].getTime() / 1000,
+      max: dateRange[1].getTime() / 1000,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <DateRangePicker
+        disableCalendar
+        rangeDivider=" to "
+        onChange={onChangeDateRange}
+        value={dateRange}
+        minDate={new Date(min * 1000)}
+        maxDate={new Date(max * 1000)}
+      />
+      <button as="input" type="submit" aria-label="Filter">
+        Filter
+      </button>
+    </form>
+  );
+};
 
 DateRangeInputCore.propTypes = {
   currentRefinement: PropTypes.object.isRequired,
   refine: PropTypes.func.isRequired,
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
 };
 
 const DateRangeInput = connectRange(DateRangeInputCore);
