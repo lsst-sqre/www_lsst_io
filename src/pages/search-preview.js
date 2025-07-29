607b6d72e0faa23bf37aae1cb0109dcb50647a85
Jonathan Sick <jsick@lsst.org>
2025-07-29 13:40:32 -0400

    Add a /search-preview page
    This page will host the demo of Vertex AI search with the embedded
    widget.

---
+++ src/pages/search-preview.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const SearchPreviewPage = () => (
+  <Layout>
+    <SEO title="Search Preview" />
+    <h1>Next-generation search preview</h1>
+
+    <p>
+      Preview the next-generation search experience for the Rubin Observatory
+      technical documentation. This page is a work in progress and may not
+      reflect the final design or functionality.
+    </p>
+  </Layout>
+);
+
+SearchPreviewPage.propTypes = {};
+
+export default SearchPreviewPage;
